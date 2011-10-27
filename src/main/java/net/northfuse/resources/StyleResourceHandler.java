eb039e8819da3b47e2b24ffef48f82cdfa5ef830
Tyler Southwick <tyler@northfuse.net>
2011-10-27 14:29:45 -0600

    disable css minifing until it works

--- src/main/java/net/northfuse/resources/StyleResourceHandler.java
+++ src/main/java/net/northfuse/resources/StyleResourceHandler.java
@@ -1,8 +1,10 @@
 package net.northfuse.resources;
 
+/*
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.yahoo.platform.yui.compressor.CssCompressor;
+*/
 import org.springframework.http.MediaType;
 
 import java.io.*;
@@ -12,7 +14,7 @@ import java.io.*;
  */
 public final class StyleResourceHandler extends ResourceHandlerImpl {
 
-	private static final Logger LOG = LoggerFactory.getLogger(StyleResourceHandler.class);
+	//private static final Logger LOG = LoggerFactory.getLogger(StyleResourceHandler.class);
 
 	/**
 	 * Creates a StyleResourceHandler.
@@ -26,10 +28,14 @@ public final class StyleResourceHandler extends ResourceHandlerImpl {
 	 */
 	@Override
 	protected InputStream wrapWithMinify(InputStream is) throws IOException {
+		return is;
+		/*
 		CssCompressor compressor = new CssCompressor(new InputStreamReader(is));
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PrintWriter writer = new PrintWriter(baos);
 		compressor.compress(writer, 0);
+		writer.close();
 		return new ByteArrayInputStream(baos.toByteArray());
+		*/
 	}
 }
